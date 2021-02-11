5dd3398b58a2b2f47c07cf83d9e5ce42519c7f7b
igoradamenko <mail@igoradamenko.com>
2021-02-11 16:36:40 +0200

    Add debug logging

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ module.exports = (options = {}) => {
   return {
     name: 'webpack-bridge',
     setup(build) {
+      log('Setup, rules number found:', rules.length);
+
       for (let i = 0; i < rules.length; i++) {
         const meta = buildRuleMeta(rules[i]);
         registerRuleOnResolve(meta, build);
@@ -19,26 +21,39 @@ module.exports = (options = {}) => {
 
 let ruleCounter = 1;
 function buildRuleMeta(rule) {
+  log('Build meta for rule', ruleCounter);
+
   const supportedRuleProperties = ['test', 'use'];
 
   if (Object.keys(rule).some(key => !supportedRuleProperties.includes(key))) {
     throw new Error(`Rule properties other than '${supportedRuleProperties.join(', ')}' are not supported yet.`);
   }
 
+  const namespace = `rule-${ruleCounter++}-${rule.test.toString()}`;
+
+  log('Generated namespace for the rule:', namespace);
+
   return {
-    namespace: `rule-${ruleCounter++}-${rule.test.toString()}`,
+    namespace,
     test: rule.test,
     use: rule.use,
   };
 }
 
 function registerRuleOnResolve(ruleMeta, build) {
+  log('Register onResolve for the rule with namespace', ruleMeta.namespace);
+
   if (ruleMeta.test instanceof RegExp) {
+    log(ruleMeta.namespace, 'is regexp rule');
+
     build.onResolve({ filter: ruleMeta.test, namespace: 'file' }, buildResolveCallback(ruleMeta));
     return;
   }
 
   if (typeof ruleMeta.test === 'string') {
+    log(ruleMeta.namespace, 'is string rule');
+
+    // TODO: do we need ^ & $ here?
     const re = new RegExp(`^${escapeRegExp(ruleMeta.test)}$`);
 
     build.onResolve({ filter: re, namespace: 'file' }, buildResolveCallback(ruleMeta));
@@ -50,7 +65,11 @@ function registerRuleOnResolve(ruleMeta, build) {
 }
 
 function buildResolveCallback(ruleMeta) {
+  log('Build onResolve callback for rule with namespace', ruleMeta.namespace);
+
   return args => {
+    log('Run onResolve callback for file', args.path, 'with namespace', ruleMeta.namespace);
+
     if (args.path.includes('!')) {
       throw new Error(`Can not load '${args.path}'. Inline loaders are not supported yet.`);
     }
@@ -65,12 +84,18 @@ function buildResolveCallback(ruleMeta) {
 }
 
 function registerRuleOnLoad(ruleMeta, build) {
+  log('Register onLoad for the rule with namespace', ruleMeta.namespace);
+
   build.onLoad({ filter: /.*/, namespace: ruleMeta.namespace }, async (args) => new Promise(resolve => {
+    log('Run loaders for', args.path, 'using rule with namespace', ruleMeta.namespace);
+
     runLoaders({
       resource: args.path,
       loaders: ruleMeta.use,
     }, (err, res) => {
       if (err) {
+        log('Error occurred while running loaders for', args.path, 'using rule with namespace', ruleMeta.namespace, '—', err);
+
         // TODO: add more info?
         // https://nodejs.org/api/errors.html
         // https://github.com/webpack/loader-runner/blob/master/lib/LoaderLoadingError.js
@@ -82,6 +107,8 @@ function registerRuleOnLoad(ruleMeta, build) {
           }],
         });
       }
+
+      log('Complete running loaders for', args.path, 'using rule with namespace', ruleMeta.namespace);
 
       return resolve({
         // TODO: https://github.com/webpack/loader-runner
@@ -96,4 +123,10 @@ function registerRuleOnLoad(ruleMeta, build) {
 function escapeRegExp(string) {
   // $& means the whole matched string
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function log(...args) {
+  if (process.env.DEBUG) {
+    console.log('[webpack-bridge]', ...args);
+  }
 }
