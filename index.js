39d9a92fe1f2c490dfce6b690a49af2ad1385430
igoradamenko <mail@igoradamenko.com>
2021-02-11 15:56:11 +0200

    Add linter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const { runLoaders } = require('loader-runner');
 
-module.exports = function(options = {}) {
+module.exports = (options = {}) => {
   const { module: { rules = [] } = {} } = options;
 
   return {
@@ -13,9 +13,9 @@ module.exports = function(options = {}) {
         registerRuleOnResolve(meta, build);
         registerRuleOnLoad(meta, build);
       }
-    }
+    },
   };
-}
+};
 
 let ruleCounter = 1;
 function buildRuleMeta(rule) {
@@ -29,7 +29,7 @@ function buildRuleMeta(rule) {
     namespace: `rule-${ruleCounter++}-${rule.test.toString()}`,
     test: rule.test,
     use: rule.use,
-  }
+  };
 }
 
 function registerRuleOnResolve(ruleMeta, build) {
@@ -45,14 +45,14 @@ function registerRuleOnResolve(ruleMeta, build) {
     return;
   }
 
-  throw new Error(`'test' property of webpack rules should be RegExp or string. Other types are not supported yet.`);
+  throw new Error('\'test\' property of webpack rules should be RegExp or string. Other types are not supported yet.');
   // console.warn('`test` property of webpack rules should be RegExp. Other types make ESBuild slower. Read more: https://esbuild.github.io/plugins/#filters');
 }
 
 function buildResolveCallback(ruleMeta) {
   return args => {
     if (args.path.includes('!')) {
-      throw new Error(`Can not load '${args.path}'. Inline loaders are not supported yet.`)
+      throw new Error(`Can not load '${args.path}'. Inline loaders are not supported yet.`);
     }
 
     ruleMeta.resolveDir = args.resolveDir; // TODO: make it cleaner?
@@ -60,39 +60,37 @@ function buildResolveCallback(ruleMeta) {
     return {
       path: path.resolve(args.resolveDir, args.path),
       namespace: ruleMeta.namespace,
-    }
-  }
+    };
+  };
 }
 
 function registerRuleOnLoad(ruleMeta, build) {
-  build.onLoad({ filter: /.*/, namespace: ruleMeta.namespace }, async (args) => {
-    return new Promise(resolve => {
-      runLoaders({
-        resource: args.path,
-        loaders: ruleMeta.use,
-      }, (err, res) => {
-        if (err) {
-          // TODO: add more info?
-          // https://nodejs.org/api/errors.html
-          // https://github.com/webpack/loader-runner/blob/master/lib/LoaderLoadingError.js
-
-          return resolve({
-            errors: [{
-              text: err.message,
-              detail: err,
-            }],
-          });
-        }
+  build.onLoad({ filter: /.*/, namespace: ruleMeta.namespace }, async (args) => new Promise(resolve => {
+    runLoaders({
+      resource: args.path,
+      loaders: ruleMeta.use,
+    }, (err, res) => {
+      if (err) {
+        // TODO: add more info?
+        // https://nodejs.org/api/errors.html
+        // https://github.com/webpack/loader-runner/blob/master/lib/LoaderLoadingError.js
 
         return resolve({
-          // TODO: https://github.com/webpack/loader-runner
-          //  according to the doc result should be Buffer or String, but it's an array with String inside. why?
-          contents: res.result[0],
-          resolveDir: ruleMeta.resolveDir,
+          errors: [{
+            text: err.message,
+            detail: err,
+          }],
         });
+      }
+
+      return resolve({
+        // TODO: https://github.com/webpack/loader-runner
+        //  according to the doc result should be Buffer or String, but it's an array with String inside. why?
+        contents: res.result[0],
+        resolveDir: ruleMeta.resolveDir,
       });
-    })
-  });
+    });
+  }));
 }
 
 function escapeRegExp(string) {
